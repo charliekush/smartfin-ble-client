2b82a22b1cfd22a9ae2e50f9eb1bf66d8a24bddd
Charlie Kushelevsky <charliekushelevsky@gmail.com>
2026-05-08 15:53:04 -0700

    feat: Add file_sink.hpp for writing and loading decoded ensembles

---
+++ src/pipeline/file_sink.hpp
@@ -0,0 +1,27 @@
+/**
+ * @file file_sink.hpp
+ * @author Charlie Kushelevsky (charliekushelevsky@gmail.com)
+ * @brief ISampleSink that writes decoded ensembles to a binary ride file,
+ *        and load_ride() to unpack that file back into decoded structs.
+ * @date 2026-05-08
+ */
+
+#ifndef FILE_SINK_HPP
+#define FILE_SINK_HPP
+
+#include "sample_sink.hpp"
+#include "protocol/ensemble_types.hpp"
+
+#include <cstdio>
+#include <cstring>
+#include <optional>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+namespace sf::pipeline {
+
+
+} // namespace sf::pipeline
+
+#endif // FILE_SINK_HPP
