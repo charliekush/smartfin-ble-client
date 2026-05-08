6e2a5222894be5f485f5dbcc0e6e0069213553b1
Charlie Kushelevsky <charliekushelevsky@gmail.com>
2026-05-07 20:32:42 -0700

    refactor: Implement Sample constructor for IMU data processing

--- src/proccessing/AHRS/madgwick_ahrs.cpp
+++ src/proccessing/AHRS/madgwick_ahrs.cpp
@@ -10,6 +10,25 @@
 
 namespace sf::ahrs {
 
+    Sample::Sample(const sf::protocol::DecodedImu &imu)
+    {
+        elapsed_time_ms = imu.elapsed_time_ms;
+        time_s = imu.elapsed_time_ms * MS_TO_S;
 
+        gyro_rad_s = {
+            static_cast<double>(imu.gyro_dps[0]) * DEG_TO_RAD,
+            static_cast<double>(imu.gyro_dps[1]) * DEG_TO_RAD,
+            static_cast<double>(imu.gyro_dps[2]) * DEG_TO_RAD};
+
+        accel_g = {
+            static_cast<double>(imu.accel_ms2[0]) / G0,
+            static_cast<double>(imu.accel_ms2[1]) / G0,
+            static_cast<double>(imu.accel_ms2[2]) / G0};
+
+        mag_uT = {
+            static_cast<double>(imu.mag_uT[0]),
+            static_cast<double>(imu.mag_uT[1]),
+            static_cast<double>(imu.mag_uT[2])};
+    }
 
 } // namespace sf::ahrs
