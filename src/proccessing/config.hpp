bd2d441f38b02199cb075c4363833e80cf779014
Charlie Kushelevsky <charliekushelevsky@gmail.com>
2026-05-07 20:06:57 -0700

    refactor: Remove unused gyroscope bias constants and include directive from config

--- src/proccessing/config.hpp
+++ src/proccessing/config.hpp
@@ -8,16 +8,10 @@
 #ifndef CONFIG_HPP
 #define CONFIG_HPP
 
-#include "lin_alg.hpp"
-
 namespace CFG
 {
 /// @brief Floating point precision used throughout the processing pipeline.
 typedef double Scalar;
-
-/// @brief Default gyroscope bias in rad/s. Replace with lab-calibrated values.
-/// @todo Collect calibrated gyro bias in lab.
-constexpr math3d::Vec3 default_gyro_bias_rad_s = {0, 0, 0};
 
 } // namespace CFG
 
