99e457bf1ff290ca1b7e82235f725b4e0a85b60b
Charlie Kushelevsky <charliekushelevsky@gmail.com>
2026-05-07 20:31:25 -0700

    refactor: Add initial implementation of madgwick_ahrs.cpp with header inclusion

---
+++ src/proccessing/AHRS/madgwick_ahrs.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file madgwick_ahrs.cpp
+ * @author Charlie Kushelevsky (charliekushelevsky@gmail.com)
+ * @brief 
+ * @date 2026-05-07
+ * 
+ * 
+ */
+#include "madgwick_ahrs.hpp"
+
+#include <cmath>
+#include <numbers>
+
+namespace sf::ahrs {
+
+
+
+} // namespace sf::ahrs
