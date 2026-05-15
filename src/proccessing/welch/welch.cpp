8602252461a2e3f5498e6c3ebabfd17bea90d868
Charlie Kushelevsky <charliekushelevsky@gmail.com>
2026-05-15 13:37:09 -0700

    feat: add documentation header for welch algorithm implementation

--- src/proccessing/welch/welch.cpp
+++ src/proccessing/welch/welch.cpp
@@ -1,3 +1,10 @@
+/**
+ * @file welch.cpp
+ * @author Charlie Kushelevsky (charliekushelevsky@gmail.com)
+ * @brief implementation of welch algorithm
+ * @date 2026-05-15
+ */
+
 #include "welch.hpp"
 
 sf::welch::WelchResult sf::welch::welch(const std::vector<double> &signal, double fs,
